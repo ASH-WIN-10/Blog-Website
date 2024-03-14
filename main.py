1650fd5ff68f0652f4b193677d675ec7d70c4dc5
ASH-WIN-10 <ashvinjangid10@gmail.com>
2024-03-14 20:30:57 +0530

    removed some code

--- main.py
+++ main.py
@@ -231,7 +231,3 @@ def delete_post(post_id):
     db.session.delete(post_to_delete)
     db.session.commit()
     return redirect(url_for('get_all_posts'))
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=os.environ.get('POST', 5000), debug=True)
